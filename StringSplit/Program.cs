8b46c9281d88e22f6c34887b8ca60083e01645e3
OldBeliver <380506338004@yandex.ua>
2022-03-30 20:52:08 +0300

    Добавьте файлы проекта.

---
+++ StringSplit/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace StringSplit
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string line = "В краю дремучих ананасов упруго колосились дыни.";
+
+            string[] split = line.Split(' ', '.');
+
+            Console.WriteLine($"{String.Join("\n", split)}");
+        }
+    }
+}
